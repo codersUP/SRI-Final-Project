696d628dbfebff8f28dad58f95ff1444b974972b
kikexd <enrique06660@gmail.com>
2022-05-11 00:49:35 -0400

    move create index to parse_document

--- src/parse_document.py
+++ src/parse_document.py
@@ -22,3 +22,33 @@ def get_importants_words(text: str):
             lst.append(token)
 
     return lst
+
+
+def create_index(documents, verbose=False):
+    index = {}
+    inverse_index = {}
+
+    for document in documents:
+        if verbose:
+            print("processing document: ", document)
+
+        words = get_importants_words(get_words_from_document(document))
+
+        index[document] = {"len": len(words), "terms": {}}
+        for token in words:
+            word = token.lemma_.lower()
+
+            # adding to index
+            if word not in index[document]["terms"]:
+                index[document]["terms"][word] = 0
+            index[document]["terms"][word] += 1
+
+            # adding to inverse_index
+            if word in inverse_index:
+                if document not in inverse_index[word]:
+                    inverse_index[word][document] = 0
+            else:
+                inverse_index[word] = {document: 0}
+            inverse_index[word][document] += 1
+
+    return (index, inverse_index)
