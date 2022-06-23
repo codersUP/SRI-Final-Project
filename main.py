4b5977f6de21d3309701dad86b5c85f4cafd06e6
kikexd <enrique06660@gmail.com>
2022-06-23 16:48:38 -0400

    fix index query name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 from pprint import pprint
 
-from src.parse_query import create_inverse_index_query
+from src.parse_query import create_index_query
 from src.parse_directory import get_files_from_path_list
 from src.parse_document import create_index
 from src.vectorial_model import calculate_rank
@@ -17,9 +17,9 @@ def main():
     while True:
         query = input("query: ")
 
-        query_inverse_index = create_inverse_index_query(query)
+        query_index = create_index_query(query)
 
-        result = calculate_rank(query_inverse_index, index, 3)
+        result = calculate_rank(query_index, index, 3)
 
         pprint(result)
 
