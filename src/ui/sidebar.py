7039b5f69e0f1718d387aeda1a4ec8cc27fe866e
kikexd <enrique06660@gmail.com>
2022-06-25 09:54:31 -0400

    add calculate results when not exists

--- src/ui/sidebar.py
+++ src/ui/sidebar.py
@@ -1,8 +1,12 @@
+import json
 import streamlit as st
-from src.utils import load_index_and_inverse_index
+from src.parse_document import create_index
+from src.parse_directory import get_files_from_path_list
+from src.utils import load_index_and_inverse_index, save_index_and_inverse_index
 from src.ui.welcome import welcome
 from src.ui.vectorial_model import vectorialmodel
 from src.ui.boolean_model import booleanmodel
+import os.path
 
 
 options = ["Proyecto", "Modelo Vectorial", "Modelo Booleano"]
@@ -11,7 +15,20 @@ router = [welcome, vectorialmodel, booleanmodel]
 
 
 def callback():
-    # TODO calculate results when not exists
+    if not os.path.isfile("results.json"):
+        f = open("files_path.json")
+        json_files_path = json.load(f)
+
+        files = get_files_from_path_list(json_files_path["paths"])
+        index, inverse_index = create_index(files[:10], verbose=True)
+
+        save_index_and_inverse_index(
+            "results.json", index=index, inverse_index=inverse_index
+        )
+
+        if "documents" in st.session_state:
+            del st.session_state["documents"]
+
     if "documents" not in st.session_state:
         index, inverse_index = load_index_and_inverse_index("results.json")
         st.session_state.documents = (index, inverse_index)
