2fe3ba3a6af7a3a1b33ac1f41020c784c86f2d00
kikexd <enrique06660@gmail.com>
2022-06-27 10:11:33 -0400

    fix main not visual imports

--- main_not_visual.py
+++ main_not_visual.py
@@ -1,10 +1,10 @@
 import json
 from pprint import pprint
 
-from src.parse_query import create_index_query
+from src.vectorial.parse_query import create_index_query
 from src.parse_directory import get_files_from_path_list
-from src.parse_document import create_index
-from src.vectorial_model import calculate_rank
+from src.vectorial.parse_document import create_index
+from src.vectorial.vectorial_model import calculate_rank
 from src.utils import load_index_and_inverse_index, save_index_and_inverse_index
 from src.boolean.boolean_model import get_documents
 
@@ -14,7 +14,7 @@ def main():
     json_files_path = json.load(f)
 
     files = get_files_from_path_list(json_files_path["paths"])
-    index, inverse_index = create_index(files[:10], verbose=True)
+    index, inverse_index = create_index(files, verbose=True)
 
     save_index_and_inverse_index(
         "results.json", index=index, inverse_index=inverse_index
